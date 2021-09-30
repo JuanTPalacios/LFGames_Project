d218c2b13a7642bacdaff6336b914e90402a7118
timdh94 <timothyh94@gmail.com>
2021-09-30 14:20:37 -0400

    Changed index to use config.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRouter = require("./routes/authRoutes");
 const gameRouter = require("./routes/gamesRoutes");
 const app = express();
-const PORT = process.env.PORT || 3005;
+const { PORT } = require('./config');
 
 // implement cors
 // new .env vars
