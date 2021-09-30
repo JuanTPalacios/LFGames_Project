864ae3cf229fcde90994646af1dc8ed2048c19da
timdh94 <timothyh94@gmail.com>
2021-09-30 14:18:04 -0400

    Created config file for environment variables.

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+require('dontenv').config();
+
+const config = {
+  PORT: process.env.PORT || '3000',
+  USERNAME: process.env.USERNAME,
+  PASSWORD: process.env.PASSWORD,
+  DATABASE: process.env.DATABASE,
+  ACCESS_TOKEN_SECRET: process.env.SECRET
+};
