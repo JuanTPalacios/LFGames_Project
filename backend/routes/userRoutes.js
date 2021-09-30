bdffe87c1c089189bafe41abbd103ff28c7205bb
timdh94 <timothyh94@gmail.com>
2021-09-30 15:03:01 -0400

    Updated update user route to use the proper usercontroller function.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const authMiddleWare = require("../Middlewares/authMiddleware");
 const userController = require('../controllers/userController')
 const router = express.Router();
 
-router.patch('/user', authMiddleWare, userController.changeUserNameOrEmail );
+router.patch('/user', authMiddleWare, userController.updateUser);
 router.get('/user', authMiddleWare, userController.getUser)
 router.post('/user', userController.createUser);
 // get user, change endpoint here,
