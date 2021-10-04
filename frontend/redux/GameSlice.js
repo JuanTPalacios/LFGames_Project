29e8006ff36a2334f967227eee6d2717e6f5865b
timdh94 <timothyh94@gmail.com>
2021-10-04 10:32:24 -0400

    "linted" redux files

--- frontend/redux/GameSlice.js
+++ frontend/redux/GameSlice.js
@@ -1,83 +1,78 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
-import { getMyGames } from "../Services/FetchCalls.js/GameApi.js/GameFetch";
-import { CLIENT_ID, API_TOKEN, LOCAL_URL } from "@env";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const URL = LOCAL_URL;
-console.log('gameslice', URL)
+console.log('gameslice', URL);
 
 export const addGame = createAsyncThunk(
-  "game/addGame",
+  'game/addGame',
   async (payload, thunkApi) => {
     try {
-      const response = await fetch(URL + "games", {
-        method: "POST",
-        credentials: "include",
-        mode: "cors",
+      const response = await fetch(`${URL}games`, {
+        method: 'POST',
+        credentials: 'include',
+        mode: 'cors',
         headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + payload.token,
-          "content-type": "application/json",
+          Accept: 'application/json',
+          Authorization: `Bearer ${payload.token}`,
+          'content-type': 'application/json',
         },
         body: JSON.stringify(payload),
       });
-      let data = await response.json();
+      const data = await response.json();
       if (response.status === 200) {
         return data;
-      } else {
-        return thunkApi.rejectWithValue(data);
       }
+      return thunkApi.rejectWithValue(data);
     } catch (err) {
-      thunkApi.rejectWithValue(err.message);
+      return thunkApi.rejectWithValue(err.message);
     }
-  }
+  },
 );
 
 export const getMyGameInfo = createAsyncThunk(
-  "game/getMyGameInfo",
+  'game/getMyGameInfo',
   async (payload, thunkApi) => {
     try {
-      const response = await fetch(URL + "games", {
-        method: "GET",
-        credentials: "include",
-        mode: "cors",
+      const response = await fetch(`${URL}games`, {
+        method: 'GET',
+        credentials: 'include',
+        mode: 'cors',
         headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + payload,
-          "content-type": "application/json",
+          Accept: 'application/json',
+          Authorization: `Bearer ${payload}`,
+          'content-type': 'application/json',
         },
       });
-      let data = await response.json();
+      const data = await response.json();
       if (response.status === 200) {
         return data;
-      } else {
-        return thunkApi.rejectWithValue(data);
       }
+      return thunkApi.rejectWithValue(data);
     } catch (err) {
-      console.log("Error", err.message);
-      thunkApi.rejectWithValue(err.message);
+      console.log('Error', err.message);
+      return thunkApi.rejectWithValue(err.message);
     }
-  }
+  },
 );
 
 export const gameSlice = createSlice({
-  name: "game",
+  name: 'game',
   initialState: {
     isFetching: false,
     isSuccess: false,
     isError: false,
-    errorMessage: "",
-    isMessage: "",
+    errorMessage: '',
+    isMessage: '',
     userGames: [],
   },
   reducers: {
     clearGameState: (state) => {
       (state.isFetching = false),
-        (state.isSuccess = false),
-        (state.errorMessage = ""),
-        (userGames = []);
-      isMessage = "";
-      isError = "";
+      (state.isSuccess = false),
+      (state.errorMessage = ''),
+      (userGames = []);
+      isMessage = '';
+      isError = '';
     },
   },
   extraReducers: (builder) => {
@@ -85,11 +80,10 @@ export const gameSlice = createSlice({
       .addCase(addGame.pending, (state) => {
         state.isFetching = true;
       })
-      .addCase(addGame.fulfilled, (state, { payload }) => {
+      .addCase(addGame.fulfilled, (state) => {
         state.isFetching = false;
-
         state.isSuccess = true;
-        state.isMessage = "";
+        state.isMessage = '';
         state.errorMessage = true;
       })
       .addCase(addGame.rejected, (state, payload) => {
@@ -98,19 +92,17 @@ export const gameSlice = createSlice({
         state.isError = true;
       });
     builder
-      .addCase(getMyGameInfo.pending, (state, payload) => {
+      .addCase(getMyGameInfo.pending, (state) => {
         state.isFetching = false;
         state.isError = true;
       })
       .addCase(getMyGameInfo.fulfilled, (state, { payload }) => {
-        if (payload.length < 1) {
-        }
         state.isFetching = false;
         state.isError = false;
         state.isSuccess = true;
         state.userGames = payload;
       })
-      .addCase(getMyGameInfo.rejected, (state, payload) => {
+      .addCase(getMyGameInfo.rejected, (state) => {
         state.isFetching = false;
         state.isError = true;
       });
