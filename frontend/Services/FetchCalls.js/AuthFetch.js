8dec286fe33244408db341d88bed21b8eac32b05
timdh94 <timothyh94@gmail.com>
2021-10-04 10:38:34 -0400

    linted services

--- frontend/Services/FetchCalls.js/AuthFetch.js
+++ frontend/Services/FetchCalls.js/AuthFetch.js
@@ -1,29 +1,29 @@
-//import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { LOCAL_URL } from "@env";
+import { LOCAL_URL } from '@env';
+
 const URL = LOCAL_URL;
-console.log(URL)
+console.log(URL);
 export const signUpUser = async (api, body) => {
-  const response = await fetch(URL + 'user', {
-    method: "POST",
+  const response = await fetch(`${URL}user`, {
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "content-type": "application/json",
+      Accept: 'application/json',
+      'content-type': 'application/json',
     },
     body: JSON.stringify(body),
   });
-  return await response.json();
+  return response.json();
 };
 
 // Sign In
 
 export const signInOldUser = async (api, body) => {
-  const response = await fetch(URL + 'signin', {
-    method: "POST",
+  const response = await fetch(`${URL}signin`, {
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "content-type": "application/json",
+      Accept: 'application/json',
+      'content-type': 'application/json',
     },
     body: JSON.stringify(body),
   });
-  return await response.json();
+  return response.json();
 };
